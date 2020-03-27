008a0b20e668a62d13a6e397a517407dcef83243
Shahab Jamshidi <shahabjamshidi@Shahabs-MacBook-Pro.local>
2020-03-27 11:45:08 +1100

    Initiate project into the git

---
+++ OnPaymentReceived.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace funcApp
+{
+    public static class OnPaymentReceived
+    {
+        [FunctionName("OnPaymentReceived")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] 
+            HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            ILogger log)
+        {
+            log.LogInformation("Received a payment.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+
+            await orderQueue.AddAsync(order);
+
+            log.LogInformation($"Order {order.OrderId} , received from {order.Email} for product {order.ProductId}");
+
+            string responseMessage = "Thank you for you purchase.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
